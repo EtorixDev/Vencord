893b42b9b54d585f54c381fba44fa0d7b2aa6251
Etorix <92535668+EtorixDev@users.noreply.github.com>
2026-09-02 16:58:06 -0700

    Cursed Orbs Wrap.

--- src/equicordplugins/questify/index.tsx
+++ src/equicordplugins/questify/index.tsx
@@ -96,6 +96,10 @@ function enrolledIncompleteButton(args: { quest: Quest, size: string; }): JSX.El
     );
 }
 
+function wrapOrbsBalance(balance: String): JSX.Element {
+    return (<span style={{ fontSize: "90%" }}>{balance}</span>);
+}
+
 export default definePlugin({
     name: "Questify",
     description: "Enhance specific Quest features, disable annoyances, or completely remove Quests.",
@@ -137,6 +141,7 @@ export default definePlugin({
     sortQuests,
     stopQuestAutoComplete,
     useQuestRerender,
+    wrapOrbsBalance,
 
     patches: [
         {
@@ -296,7 +301,7 @@ export default definePlugin({
             replacement: [
                 {
                     match: /(?<=children:)(\i\?\?0)/,
-                    replace: "($1).toLocaleString(undefined,{maximumFractionDigits:0}),style:{'align-self':'center'}"
+                    replace: "$self.wrapOrbsBalance(($1).toLocaleString(undefined,{maximumFractionDigits:0}))"
                 }
             ]
         },
